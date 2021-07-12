d9c96e06f41f3d884add9bc1691ea85264f748e0
KajalMishra60 <69347123+KajalMishra60@users.noreply.github.com>
2021-07-12 12:01:18 +0530

    Update DivBetween.js

--- DivBetween.js
+++ DivBetween.js
@@ -1,32 +1,36 @@
-document.getElementById("Between").addEventListener('click', function() {
-   document.getElementById("G").insertAdjacentHTML('afterend','<div class="col">O</div>');    
+document.getElementById("Between").addEventListener('click', function() {  
+   document.getElementById("G").insertAdjacentHTML('afterend','<div class="col">O</div>');        
+   document.getElementById("Between").hidden=true;
 });
-let p=0;
+let rows,cols;
 document.getElementById("gendiv").addEventListener('click', function() {
-    let rows=window.prompt("Enter number of Rows","1");
-    let cols=window.prompt("Enter number of Column","1");
+    rows=window.prompt("Enter number of Rows","1");
+    cols=window.prompt("Enter number of Column","1");
     for(let i=0; i<rows; i++) {
         let rowDiv = document.createElement("div");
         rowDiv.className = "row";    
         for(let j=0; j<cols; j++) {
             let columnDiv = document.createElement("div");
             columnDiv.className = "col";                       
-            columnDiv.innerHTML = p;
-            columnDiv.id = p++;
+            columnDiv.id = i.toString()+j.toString();
             rowDiv.appendChild(columnDiv);
         }    
         document.getElementById("gen").appendChild(rowDiv);
     }
  });
  document.getElementById("colordiv").addEventListener('click', function() {     
-    let input=window.prompt("Enter div id");
-    if(input>=p){
+    let r=window.prompt("Enter row number");
+    if(r>=rows){
     alert("Invalid");
     }
+    else {
+        let c=window.prompt("Enter Column number");
+        if(c>=cols){
+            alert("Invalid");
+            }
     else{
         let paint=window.prompt("Enter the name of color OR hex with #");
-        document.getElementById(input).style.backgroundColor=paint;
+        document.getElementById(r.toString()+c.toString()).style.backgroundColor=paint;
     }
-   input.innerHTML="Kajal";
-    
+}
  });
